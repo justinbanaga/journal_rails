0c99ecb5dd12da4d6355d10fc631e7b85b7def17
justinbanaga <justinbanaga.195@gmail.com>
2022-02-10 10:26:14 +0800

    add uniqueness validations for email attribute at the database-level

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,8 +49,14 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email addresses should be unique' do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
