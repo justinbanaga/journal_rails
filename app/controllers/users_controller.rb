4e2751a9d72b526b34c714624989c4fc4791560f
justinbanaga <justinbanaga.195@gmail.com>
2022-02-11 17:38:13 +0800

    Setup sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[index edit update destroy]
-  def new; end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+// import { defineConfig } from 'vite'
+// import RubyPlugin from 'vite-plugin-ruby'
 
-export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+// export default defineConfig({
+//   plugins: [
+//     RubyPlugin(),
+//   ],
+// })
+
+// inside vite.config.ts
+ import { defineConfig } from 'vite'
+ import RubyPlugin from 'vite-plugin-ruby'
+ import FullReload from 'vite-plugin-full-reload'
+
+ export default defineConfig({
+   plugins: [
+     RubyPlugin(),
+     FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 })
+   ],
+ })
