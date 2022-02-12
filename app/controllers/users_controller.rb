040acfa469a8566ac0fb4acc9e31aa5d31eb4fc6
justinbanaga <justinbanaga.195@gmail.com>
2022-02-12 16:22:52 +0800

    Add tests for successful registrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # not the final implementation
     if @user.save
-      # handle success
+      flash[:success] = 'Welcome to the Journal App!'
+      redirect_to @user
     else
       render 'new'
     end
