2b331feaec5fd25043c5af2bdca73ed7aa6816fb
justinbanaga <justinbanaga.195@gmail.com>
2022-02-12 15:15:46 +0800

    Add border styling to fields with errors in the sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params) # not the final implementation
+    if @user.save
+      # handle success
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
