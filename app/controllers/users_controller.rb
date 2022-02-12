b4648b52c73bd7b683e547a1ec6ddc40a7602c1e
justinbanaga <justinbanaga.195@gmail.com>
2022-02-12 20:12:51 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # not the final implementation
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = 'Welcome to the Journal App!'
       redirect_to @user
